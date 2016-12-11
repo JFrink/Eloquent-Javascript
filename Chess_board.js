c32b0214068cd065e7efcb99e874e19d920a82e8
Justin Frink <jfrinkphotography@gmail.com>
2016-12-11 17:01:03 -0500

    Update Chess_board.js

--- Chess_board.js
+++ Chess_board.js
@@ -1,24 +1,23 @@
-var string = "";
+var string = "";  
 var size = 8;
 
 for (i = 0; i < size; i++) {
-  for (r = 0; r < size; r++) {
-    if (i % 2 == 0) {
-		if (r % 2 == 0) {
-      		string += "#";
-    	} else {
+    for (r = 0; r < size; r++) {
+        if (i % 2 == 0) {
+	   if (r % 2 == 0) {
+      	        string += "#";
+    	   } else {
       		string += " ";
-        }
+           }
   	} else {
-      if (r % 2 == 0) {
-        string += " ";
-      }
-      else {
-        string += "#"
-      }
-    }
-  }
-  string += "\n"
+     	    if (r % 2 == 0) {
+                string += " ";
+            } else {
+                string += "#"
+            }
+         }
+     }
+    string += "\n"
 }
 
 console.log(string)
